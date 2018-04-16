aee0509ade0f45a54bae5dc562301d8e94ce2d5c
khashab2 <khashab2@illinois.edu>
2018-04-16 14:15:30 -0400

    minor change to the expected output.

--- eval/multirc-eval-v1.py
+++ eval/multirc-eval-v1.py
@@ -1,6 +1,16 @@
+import json
+from pprint import pprint
 
 def main():
     print("asdasd")
+    eval()
+
+def eval():
+    input = json.load(open('/Users/daniel/ideaProjects/multirc/eval/sample-input.json'))
+    output = json.load(open('/Users/daniel/ideaProjects/multirc/eval/sample-output-binary.json'))
+    pprint(input)
+    pprint(output)
+
 
 if __name__ == "__main__":
     main()
