45f254091eca8f210a1eb0547f20ac1a19eb1183
khashab2 <khashab2@illinois.edu>
2018-07-11 17:21:21 -0400

    move the evaluation metrics to a different file.

---
+++ eval/measures.py
@@ -0,0 +1,71 @@
+import math
+
+
+class Measures:
+
+    @staticmethod
+    def per_question_metrics(dataset, outputMap):
+        P = []
+        R = []
+        for p in dataset:
+            for qIdx, q in enumerate(p["paragraph"]["questions"]):
+                id = p["id"] + "==" + str(qIdx)
+                if (id in outputMap):
+                    predictedAns = outputMap.get(id)
+                    correctAns = [int(a["isAnswer"]) for a in q["answers"]]
+                    predictCount = sum(predictedAns)
+                    correctCount = sum(correctAns)
+                    assert math.ceil(sum(predictedAns)) == sum(predictedAns), "sum of the scores: " + str(sum(predictedAns))
+                    agreementCount = sum([a * b for (a, b) in zip(correctAns, predictedAns)])
+                    p1 = (1.0 * agreementCount / predictCount) if predictCount > 0.0 else 1.0
+                    r1 = (1.0 * agreementCount / correctCount) if correctCount > 0.0 else 1.0
+                    P.append(p1)
+                    R.append(r1)
+                else:
+                    print("The id " + id + " not found . . . ")
+
+        pAvg = Measures.avg(P)
+        rAvg = Measures.avg(R)
+        f1Avg = 2 * Measures.avg(R) * Measures.avg(P) / (Measures.avg(P) + Measures.avg(R))
+        return [pAvg, rAvg, f1Avg]
+
+    @staticmethod
+    def exact_match_metrics(dataset, outputMap, delta):
+        EM = []
+        for p in dataset:
+            for qIdx, q in enumerate(p["paragraph"]["questions"]):
+                id = p["id"] + "==" + str(qIdx)
+                if (id in outputMap):
+                    predictedAns = outputMap.get(id)
+                    correctAns = [int(a["isAnswer"]) for a in q["answers"]]
+                    em = 1.0 if sum([abs(i - j) for i, j in zip(correctAns, predictedAns)]) <= delta  else 0.0
+                    EM.append(em)
+                else:
+                    print("The id " + id + " not found . . . ")
+
+        return Measures.avg(EM)
+
+    @staticmethod
+    def per_dataset_metric(dataset, outputMap):
+        agreementCount = 0
+        correctCount = 0
+        predictCount = 0
+        for p in dataset:
+            for qIdx, q in enumerate(p["paragraph"]["questions"]):
+                id = p["id"] + "==" + str(qIdx)
+                if (id in outputMap):
+                    predictedAns = outputMap.get(id)
+                    correctAns = [int(a["isAnswer"]) for a in q["answers"]]
+                    predictCount += sum(predictedAns)
+                    correctCount += sum(correctAns)
+                    agreementCount += sum([a * b for (a, b) in zip(correctAns, predictedAns)])
+                else:
+                    print("The id " + id + " not found . . . ")
+
+        p1 = (1.0 * agreementCount / predictCount) if predictCount > 0.0 else 1.0
+        r1 = (1.0 * agreementCount / correctCount) if correctCount > 0.0 else 1.0
+        return [p1, r1, 2 * r1 * p1 / (p1 + r1)]
+
+    @staticmethod
+    def avg(l):
+        return reduce(lambda x, y: x + y, l) / len(l)
--- eval/multirc-eval-v1.py
+++ eval/multirc-eval-v1.py
@@ -1,16 +1,22 @@
+### Evaluation script used for evaluation of baselines for MultiRC dataset
+# The evaluation script expects the questions, and predicted answers from separate json files.
+# The predicted answers should be 1s and 0s (no real-valued scores)
+
 import json
+from measures import Measures
 
 # this is the location of your data; has to be downloaded from http://cogcomp.org/multirc/
-inputFile = '/Users/daniel/ideaProjects/hard-qa/split/dev_83.json'
+inputFile = '/Users/daniel/ideaProjects/hard-qa/splitv1/dev_83-with-lucene.json'
 
+
+measures = Measures()
 
 def main():
+
     eval('baseline-scores/human-01.json')
-
-
-def avg(l):
-    return reduce(lambda x, y: x + y, l) / len(l)
-
+    # eval('baseline-scores/allOnes.json')
+    # eval('baseline-scores/allZeros.json')
+    # eval('baseline-scores/simpleLR.json')
 
 def eval(outFile):
     input = json.load(open(inputFile))
@@ -19,49 +25,19 @@ def eval(outFile):
 
     assert len(outputMap) == len(output), "You probably have redundancies in your keys"
 
-    P1 = []
-    R1 = []
-
-    for p in input["data"]:
-        for qIdx, q in enumerate(p["paragraph"]["questions"]):
-            id = p["id"] + "==" + str(qIdx)
-            if (id in outputMap):
-                predictedAns = outputMap.get(id)
-                correctAns = [int(a["isAnswer"]) for a in q["answers"]]
-                predictCount = sum(predictedAns)
-                correctCount = sum(correctAns)
-                agreementCount = sum([a * b for (a, b) in zip(correctAns, predictedAns)])
-                p1 = (1.0 * agreementCount / predictCount) if predictCount > 0.0 else 1.0
-                r1 = (1.0 * agreementCount / correctCount) if correctCount > 0.0 else 1.0
-                P1.append(p1)
-                R1.append(r1)
-            else:
-                print("The id " + id + " not found . . . ")
-
+    [P1, R1, F1m] = measures.per_question_metrics(input["data"], outputMap)
     print("Per question measures (i.e. precision-recall per question, then average) ")
-    print("\tP: " + str(avg(P1)) + " - R: " + str(avg(R1)) + " - F1m: " + str(2 * avg(R1) * avg(P1) / (avg(P1) + avg(R1))))
+    print("\tP: " + str(P1) + " - R: " + str(R1) + " - F1m: " + str(F1m))
 
-    agreementCount = 0
-    correctCount = 0
-    predictCount = 0
-    for p in input["data"]:
-        for qIdx, q in enumerate(p["paragraph"]["questions"]):
-            id = p["id"] + "==" + str(qIdx)
-            if (id in outputMap):
-                predictedAns = outputMap.get(id)
-                correctAns = [int(a["isAnswer"]) for a in q["answers"]]
-                predictCount += sum(predictedAns)
-                correctCount += sum(correctAns)
-                agreementCount += sum([a * b for (a, b) in zip(correctAns, predictedAns)])
-            else:
-                print("The id " + id + " not found . . . ")
+    EM0 = measures.exact_match_metrics(input["data"], outputMap, 0)
+    EM1 = measures.exact_match_metrics(input["data"], outputMap, 1)
+    print("\tEM0: " + str(EM0))
+    print("\tEM1: " + str(EM1))
 
-    p1 = (1.0 * agreementCount / predictCount) if predictCount > 0.0 else 1.0
-    r1 = (1.0 * agreementCount / correctCount) if correctCount > 0.0 else 1.0
+    [P2, R2, F1a] = measures.per_dataset_metric(input["data"], outputMap)
 
     print("Dataset-wide measures (i.e. precision-recall across all the candidate-answers in the dataset) ")
-    print("\tP: " + p1) + " - R: " + str(r1) + " - F1a: " + str(2 * r1 * p1 / (p1 + r1))
-
+    print("\tP: " + str(P2) + " - R: " + str(R2) + " - F1a: " + str(F1a))
 
 if __name__ == "__main__":
     main()
